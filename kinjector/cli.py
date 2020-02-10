e5d91eca11c9fda884d561ee123ce6a0b62f10bc
xesscorp <xesscorp@xess.com>
2020-02-10 17:17:55 -0500

    Reset file pointer from EOF or else a board file will look like legal YAML and gets overwritten with YAML.

--- kinjector/cli.py
+++ kinjector/cli.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import collections
+import json
+import logging
 import os
 import shutil
 import sys
-import logging
-import collections
-import json
-import yaml
+
 import pcbnew
+import yaml
+
 from .kinjector import *
 from .pckg_info import version
 
@@ -17,53 +19,59 @@ def main():
     """Command-line interface."""
 
     parser = argparse.ArgumentParser(
-        description=
-        '''Inject/eject JSON/YAML data to/from a KiCad project file.''')
-
-    parser.add_argument('--version',
-                        '-v', '-V',
-                        action='version',
-                        version='kinjector ' + version)
+        description="""Inject/eject JSON/YAML data to/from a KiCad project file."""
+    )
 
     parser.add_argument(
-        '--from_',
-        '-f',
-        nargs='+',
+        "--version", "-v", "-V", action="version", version="kinjector " + version
+    )
+
+    parser.add_argument(
+        "--from_",
+        "-f",
+        nargs="+",
         type=str,
-        metavar='file.[json|yaml|kicad_pcb]',
-        help='''Extract values from one or more JSON/YAML/KiCad files.''')
+        metavar="file.[json|yaml|kicad_pcb]",
+        help="""Extract values from one or more JSON/YAML/KiCad files.""",
+    )
 
     parser.add_argument(
-        '--to',
-        '-t',
-        nargs='+',
+        "--to",
+        "-t",
+        nargs="+",
         type=str,
-        metavar='file.[json|yaml|kicad_pcb]',
-        help='''Insert values into one or more JSON/YAML/KiCad files.''')
-
-    parser.add_argument('--overwrite',
-                        '-w',
-                        action='store_true',
-                        help='Allow value insertion into an existing file.')
-
-    parser.add_argument('--nobackup',
-                        '-nb',
-                        action='store_true',
-                        help='''Do *not* create backups before modifying files.
-            (Default is to make backup files.)''')
+        metavar="file.[json|yaml|kicad_pcb]",
+        help="""Insert values into one or more JSON/YAML/KiCad files.""",
+    )
 
     parser.add_argument(
-        '--debug',
-        '-d',
-        nargs='?',
+        "--overwrite",
+        "-w",
+        action="store_true",
+        help="Allow value insertion into an existing file.",
+    )
+
+    parser.add_argument(
+        "--nobackup",
+        "-nb",
+        action="store_true",
+        help="""Do *not* create backups before modifying files.
+            (Default is to make backup files.)""",
+    )
+
+    parser.add_argument(
+        "--debug",
+        "-d",
+        nargs="?",
         type=int,
         default=0,
-        metavar='LEVEL',
-        help='Print debugging info. (Larger LEVEL means more info.)')
+        metavar="LEVEL",
+        help="Print debugging info. (Larger LEVEL means more info.)",
+    )
 
     args = parser.parse_args()
 
-    logger = logging.getLogger('kinjector')
+    logger = logging.getLogger("kinjector")
     if args.debug is not None:
         log_level = logging.DEBUG + 1 - args.debug
         handler = logging.StreamHandler(sys.stdout)
@@ -72,26 +80,28 @@ def main():
         logger.setLevel(log_level)
 
     if args.from_ is None:
-        logger.critical('Hey! Give me some files to extract from!')
+        logger.critical("Hey! Give me some files to extract from!")
         sys.exit(2)
 
     if args.to is None:
-        print('Hey! I need some files where I can insert values!')
+        print("Hey! I need some files where I can insert values!")
         sys.exit(1)
 
     for file in args.to:
         if os.path.isfile(file):
             if not args.overwrite and args.nobackup:
                 logger.critical(
-                    '''File {} already exists! Use the --overwrite option to
-                    allow modifications to it or allow backups.'''.format(
-                        file))
+                    """File {} already exists! Use the --overwrite option to
+                    allow modifications to it or allow backups.""".format(
+                        file
+                    )
+                )
                 sys.exit(1)
             if not args.nobackup:
                 # Create a backup file.
                 index = 1  # Start with this backup file suffix.
                 while True:
-                    backup_file = file + '.{}.bak'.format(index, file)
+                    backup_file = file + ".{}.bak".format(index, file)
                     if not os.path.isfile(backup_file):
                         # Found an unused backup file name, so make backup.
                         shutil.copy(file, backup_file)
@@ -101,7 +111,7 @@ def main():
     # Combine the input files into a single injection dict.
     injection_dict = {}
     for file in args.from_:
-        fp = open(file, 'r')  # Throw exception if file doesn't exist.
+        fp = open(file, "r")  # Throw exception if file doesn't exist.
         try:
             # Do this if it's a JSON file.
             fp.seek(0)
@@ -129,28 +139,32 @@ def main():
     # Insert the injection dict into each of the output files.
     for file in args.to:
         try:
-            fp = open(file, 'r')
+            fp = open(file, "r")
 
             # The file exists and could be opened, now find out what type it is.
             try:
                 fp.seek(0)
-                file_dict = json.load(fp) # Causes exception if not JSON.
+                file_dict = json.load(fp)  # Causes exception if not JSON.
                 fp.close()
                 # Overwrite the JSON file.
-                with open(file, 'w') as fp:
+                with open(file, "w") as fp:
                     json.dump(injection_dict, fp, indent=4)
             except Exception:
                 try:
                     fp.seek(0)
-                    file_dict = yaml.load(fp, Loader=yaml.Loader) # Exception if not YAML.
+                    file_dict = yaml.load(
+                        fp, Loader=yaml.Loader
+                    )  # Exception if not YAML.
                     fp.close()
                     # Overwrite the YAML file.
-                    with open(file, 'w') as fp:
+                    with open(file, "w") as fp:
                         yaml.safe_dump(injection_dict, fp, default_flow_style=False)
                 except Exception:
                     try:
                         fp.close()
-                        brd = pcbnew.LoadBoard(file) # Exception if not KiCad board file.
+                        brd = pcbnew.LoadBoard(
+                            file
+                        )  # Exception if not KiCad board file.
                         # Inject the new values into the board.
                         Board().inject(injection_dict, brd)
                         # Overwrite the KiCad board file.
@@ -160,11 +174,11 @@ def main():
         except IOError:
             # No existing file, so create a file with the injection dict contents.
             file_ext = str.lower(os.path.splitext(file)[1])
-            if file_ext == '.json':
-                with open(file,'w') as fp:
+            if file_ext == ".json":
+                with open(file, "w") as fp:
                     json.dump(injection_dict, fp, indent=4)
-            elif file_ext == '.yaml':
-                with open(file,'w') as fp:
+            elif file_ext == ".yaml":
+                with open(file, "w") as fp:
                     yaml.safe_dump(injection_dict, fp, default_flow_style=False)
             else:
                 Board().inject(injection_dict, brd)
@@ -174,5 +188,5 @@ def main():
 ###############################################################################
 # Main entrypoint.
 ###############################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
